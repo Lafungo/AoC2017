59b4519f49fa6d1df7a426fa979e54e0842234a1
Jonathan Toole <34099439+Lafungo@users.noreply.github.com>
2017-12-14 15:45:14 -0500

    Fix day 3 distance calculation and add position function for more flexibility

--- d3.py
+++ d3.py
@@ -5,32 +5,41 @@ Created on Wed Dec 13 08:40:34 2017
 @author: Lafungo
 """
 
-def dist(n):
+def position(n):
     if n == 1:
-        return 0
+        return (0, 0)
     
     i = 1
     
     while (i + 2)**2 < n:
         i += 2
-        
-    d = i // 2 # minimum distance
+    
+    d = i // 2 # square on diagonal at (d, -d)
     m = n - i**2
     
     # switch per quadrant (based on diagonal axes)
     if m <= i + 1:
-        d += abs(m - d)
+        x = d + 1
+        y = (m - 1) - d
     elif m <= 2 * (i + 1):
         m -= i + 1
-        d += abs(m - d)
+        x = d + 1 - m
+        y = d + 1
     elif m <= 3 * (i + 1):
         m -= 2 * (i + 1)
-        d += abs(m - d)
+        x = -(d + 1)
+        y = d + 1 - m
     else:
         m -= 3 * (i + 1)
-        d += abs(m - d)
+        x = -(d + 1) + m
+        y = -(d + 1)
         
-    return d
+    return (x, y)
+        
+
+def dist(n):
+    x, y = position(n)
+    return abs(x) + abs(y)
 
 if __name__ == '__main__':
     while True:
