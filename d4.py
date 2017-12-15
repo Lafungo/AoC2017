2ef7c69f3d965bf8e9a52d841208d78f235f887a
Jonathan Toole <34099439+Lafungo@users.noreply.github.com>
2017-12-15 09:19:34 -0500

    Add day 4

---
+++ d4.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 08:45:00 2017
+
+@author: Lafungo
+"""
+
+def validate(p):
+    t = p.strip().split(' ')
+    s = set(t)
+    
+    if len(t) < 2:
+        return False
+    
+    if len(s) == len(t):
+        return True
+    else:
+        return False
+    
+if __name__ == '__main__':
+    with open('d4_input.txt') as f:
+        s = 0
+        
+        for line in f:
+            if validate(line):
+                s += 1
+                
+        print(s)
